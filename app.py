07115cce908e5002a7fd52cbfd8632ccd6050b08
Alexong8 <148948532+Alexong8@users.noreply.github.com>
2024-01-03 18:12:29 +0800

    Add purchase feature

--- app.py
+++ app.py
@@ -87,3 +87,18 @@ def view_cart():
     else:
         flash('Please log in to view your cart.', 'danger')
         return redirect(url_for('login'))
+
+@app.route('/purchase')
+def purchase():
+    if 'logged_in' in session and session['logged_in']:
+        user_cart = users[session['username']]["cart"]
+        total_price = sum(book["price"] for book in user_cart)
+        users[session['username']]["cart"] = []
+        flash(f'Purchase successful! Total: ${total_price}', 'success')
+    else:
+        flash('Please log in to make a purchase.', 'danger')
+
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
