e14f2bac15b6f880dc3ef5d9abbbc56b18e9214b
Jinhong426 <148414200+Jinhong426@users.noreply.github.com>
2024-01-03 18:11:02 +0800

    Add Cart feature

--- app.py
+++ app.py
@@ -64,3 +64,26 @@ def logout():
     session.pop('username', None)
     flash('Logout successful!', 'success')
     return redirect(url_for('index'))
+
+@app.route('/add_to_cart/<int:book_id>')
+def add_to_cart(book_id):
+    if 'logged_in' in session and session['logged_in']:
+        book = next((b for b in books if b['id'] == book_id), None)
+        if book:
+            users[session['username']]["cart"].append(book)
+            flash(f'{book["title"]} added to your cart!', 'success')
+        else:
+            flash('Book not found.', 'danger')
+    else:
+        flash('Please log in to add items to your cart.', 'danger')
+
+    return redirect(url_for('index'))
+
+@app.route('/cart')
+def view_cart():
+    if 'logged_in' in session and session['logged_in']:
+        user_cart = users[session['username']]["cart"]
+        return render_template('cart.html', cart=user_cart)
+    else:
+        flash('Please log in to view your cart.', 'danger')
+        return redirect(url_for('login'))
